f6dacf4e5655bce4c0d482107cfa991e79d99296
smetdenis <admin@jbzoo.com>
2014-09-04 22:39:56 +0700

    .no-js + .js-ready feature

--- js/template.js
+++ js/template.js
@@ -1,13 +1,15 @@
 // check and define $ as jQuery
 if (typeof jQuery != "undefined") jQuery(function ($) {
 
+    // remove no-js class if JavaScript enabled
+    $('html.no-js').removeClass('no-js').addClass('js-ready');
+
     // Close Joomla system messages (for example)
     $('#system-message .close').click(function () {
 
-        $(this).closest('.alert')
-            .animate({ height: 0, opacity: 0 }, 'slow', function () {
-                $(this).remove();
-            });
+        $(this).closest('.alert').animate({height: 0, opacity: 0}, 'slow', function () {
+            $(this).remove();
+        });
         return false;
     });
 
