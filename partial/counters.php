f9fcebb1677d4a95149f43dc15e5a1b1129036d5
smetdenis <admin@jbzoo.com>
2014-09-08 17:21:32 +0700

    Add spaces

--- partial/counters.php
+++ partial/counters.php
@@ -1,4 +1,6 @@
+
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+
 <script>
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
     function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
