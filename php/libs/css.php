1ec79ce384e093e7799cce09bc43464d34f6a6ee
smetdenis <admin@jbzoo.com>
2014-09-18 03:39:39 +0700

    Update cache checker

--- php/libs/css.php
+++ php/libs/css.php
@@ -43,11 +43,6 @@ abstract class JBlankCss
     protected $_path = null;
 
     /**
-     * @var string
-     */
-    protected $_cacheMix = '';
-
-    /**
      * @param JBlankTemplate $tpl
      */
     public function __construct(JBlankTemplate $tpl)
@@ -100,30 +95,42 @@ abstract class JBlankCss
         $file     = JPath::clean($file);
         $filename = pathinfo($file, PATHINFO_FILENAME);
 
-        $hash      = (($debug) ? ('debug-' . $filename) : $this->_getHash($file, $this->_path, $this->_filter));
-        $path      = '/cache/jblank/styles-' . $hash . '-' . $this->type . '.css';
+        $hash      = $this->_getHash($file, $this->_path, $this->_filter);
+        $path      = '/cache/jblank/styles-' . $filename . '-' . $this->type . ($debug ? '-debug' : '') . '.css';
         $relPath   = rtrim($this->_tpl->baseurl, '/') . $path;
         $cachePath = JPath::clean(JPATH_ROOT . '/' . $path);
 
-        if ($debug) {
-            // build it always
-            $css = $this->_compile($file);
-            $this->_save($cachePath, $css);
-            $relPath .= '?' . $random; // forced disable browser cache
+        $updateFile = false;
+        if (JFile::exists($cachePath)) {
+
+            // quickest way for getting first file line
+            $cacheRes  = fopen($cachePath, 'r');
+            $firstLine = fgets($cacheRes);
+            fclose($cacheRes);
+
+            // check cacheid
+            if (!preg_match('#' . $hash . '#i', $firstLine)) {
+                $updateFile = true;
+            }
 
         } else {
-            // check is cached fils exists
-            if (!JFile::exists($cachePath) && $css = $this->_compile($file)) {
-                $this->_save($cachePath, $css);
-            }
+            $updateFile = true;
+        }
+
+        if ($updateFile) {
+            $css = $this->_compile($file);
+            $css = '/* cacheid:' . $hash . " */\n" . $css;
+            $this->_save($cachePath, $css);
         }
 
         if (filesize($cachePath) > 5) {
-            return $relPath;
+            $mtime = substr(filemtime($cachePath), -3);
+            return $relPath . '?' . $mtime;
         }
 
         return null;
     }
+
 
     /**
      * @return bool
@@ -143,10 +150,10 @@ abstract class JBlankCss
     {
         $hash = array(
             '_file'   => $file,
+            '_type'   => $this->type,
             '_dir'    => $this->_path,
             '_filter' => $this->_filter,
-            '_mix'    => $this->_cacheMix,
-            '_debug'  => $this->_isDebug(),
+            '_debug'  => $this->_isDebug() ? '1' : '0',
         );
 
         if ($path) {
